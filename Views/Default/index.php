2f5d13bd885233c30b9146c49ed65403f4884f0e
Khoa Nguyen <nguyentrananh_khoa@yahoo.com>
2015-11-01 19:51:43 +0700

    modify default UI

--- Views/Default/index.php
+++ Views/Default/index.php
@@ -1,10 +1,30 @@
+<!DOCTYPE html>
 <html>
+<title>Personio Assignment</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Keywords" content="">
 <head>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="../../Resources/css/bootstrap.min.css">
 
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="../../Resources/css/bootstrap-theme.min.css">
 </head>
 <body>
-Default controller <br />
-
-Go to <a href="/calculation">Calculate remaining vacation of employee</a>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">Remaining vacation calculation</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8">
+            Go to <a href="/calculation">Calculate remaining vacation of employee</a>
+        </div>
+        <div class="col-md-2"></div>
+    </div>
+</div>
 </body>
 </html>
