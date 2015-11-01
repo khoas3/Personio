1316f9dffdcea78dc2abda6d5584d51be543d5bf
Khoa Nguyen <nguyentrananh_khoa@yahoo.com>
2015-11-01 15:54:52 +0700

    Fixed dateDiff issue

--- Controllers/Calculation.php
+++ Controllers/Calculation.php
@@ -3,23 +3,30 @@
 //namespace Personio\Controllers;
 
 class CalculationController extends BaseController {
+    /**
+     * @param $action
+     * @param $urlValues
+     */
     public function __construct($action, $urlValues)
     {
         parent::__construct($action, $urlValues);
     }
 
+    /**
+     * Calculate employee's vacation.
+     */
     public function index()
     {
         // If method is POST
         if (!empty($_POST))
         {
             if($_POST['hire_date'] && $_POST['calculation_date']){
-                $s = new \DateTime($_POST['hire_date']);
-                $e = new \DateTime($_POST['calculation_date']);
-                $c = new Vacation($s, $e);
-                $c->calculate();
-                $vacation = $c->getVacation();
-                echo json_encode($vacation);
+                $hire_date = trim($_POST['hire_date']);
+                $calc_date = trim($_POST['calculation_date']);
+                $vacation = new Vacation($hire_date, $calc_date);
+                $vacation->calculate();
+                $result = $vacation->getVacation();
+                echo json_encode($result);
             }
             exit(die);
         }
--- Libraries/Vacation.php
+++ Libraries/Vacation.php
@@ -7,6 +7,7 @@ class Vacation {
 	const VACATION_FULL_YEAR = 24;
 	const VACATION_PER_MONTH = 2;
 	const PROBATION_MONTH_PERIOD = 6;
+    const DATE_TO_REMOVE = '04-01'; // m-d (Vacation will be lost from 1st of April 2016 forward.)
 
     /**
      * @var
@@ -32,13 +33,16 @@ class Vacation {
      * @param DateTime $start_date
      * @param DateTime $calc_date
      */
-	public function __construct(\DateTime $start_date, \DateTime $calc_date)
+	public function __construct($start_date, $calc_date)
 	{
-		if ($start_date > $calc_date) {
-			throw new \InvalidArgumentException('Start date cannot be greater than calculation date.');
-		}
-		$this->start_date = $start_date;
-		$this->calc_date = $calc_date;
+        // If not numeric then convert texts to unix timestamps
+        if (!is_int($start_date)) {
+            $this->start_date = strtotime($start_date);
+        }
+        if (!is_int($calc_date)) {
+            $this->calc_date = strtotime($calc_date);
+        }
+
         $this->init();
         $this->isQualified($start_date, $calc_date);
 	}
@@ -61,74 +65,128 @@ class Vacation {
     {
         return $this->vacation;
     }
-	
+
+    /**
+     * @param $year
+     * @param $num
+     * @param string $default
+     * @return $this
+     */
+    public function addVacation($year, $num, $default = 'vacation')
+    {
+        $this->vacation[$default][$year] = $num;
+        return $this;
+    }
+
+    /**
+     * @param $year
+     * @param string $default
+     * @return $this
+     */
+    public function removeVacation($year, $default = 'vacation')
+    {
+        if(isset($this->vacation[$default][$year])){
+            unset($this->vacation[$default][$year]);
+        }
+    }
+
+    /**
+     * @param $start_date
+     * @param $calc_date
+     */
+    private function clearVacation($start_date, $calc_date)
+    {
+        $start_year = date('Y', $start_date );
+        $calc_year = date('Y', $calc_date );
+        $range_of_year = $calc_year - $start_year;
+        $endpoint = strtotime($calc_year.'-'.self::DATE_TO_REMOVE);
+        if($range_of_year > 0){
+            // Calc date greater than or equal endpoint.
+            if($calc_date - $endpoint >= 0){
+                for($i=$calc_year-1;$i>=$start_year; $i--){
+                    $this->removeVacation($i);
+                }
+            }else{
+                // year period greater than or equal 1
+                $interval = $this->dateDiff($start_date, $calc_date);
+                if($interval >= 1){
+                    for($i=$calc_year-2; $i>=$start_year;$i--){
+                        $this->removeVacation($i);
+                    }
+                }
+            }
+        }
+    }
 	/**
      * Void main
      */
     public function calculate()
     {
-        return $this->accumulatedVacation($this->start_date, $this->calc_date);
+        // Calculate num of vacation.
+        $this->accumulatedVacation($this->start_date, $this->calc_date);
+        // Vacation will be lost from 1st of April 2016 forward.
+        $this->clearVacation($this->start_date, $this->calc_date);
     }
 
     /**
-     * @param DateTime $start_date
-     * @param DateTime $calc_date
+     * @param $start_date
+     * @param $calc_date
      */
-    private function isQualified(\DateTime $start_date, \DateTime $calc_date)
+    private function isQualified($start_date, $calc_date)
     {
-        $number_of_m = $this->workedMonth($start_date, $calc_date);
-        $number_of_m += 1; // Granted vacation on the first working day.
-        if($number_of_m > self::PROBATION_MONTH_PERIOD)
+        $num_of_m = $this->workedMonth($start_date, $calc_date);
+        $num_of_m += 1; // Granted vacation on the first working day.
+        if($num_of_m > self::PROBATION_MONTH_PERIOD)
         {
             $this->qualified = true;
         }
     }
 
     /**
-     * @param DateTime $start_date
-     * @param DateTime $calc_date
+     * @param $start_date
+     * @param $calc_date
      * @return int
      */
-    private function accumulatedVacation(\DateTime $start_date, \DateTime $calc_date)
+    private function accumulatedVacation($start_date, $calc_date)
     {
-        $start_year = intval($start_date->format('Y'));
-        $start_day = intval($start_date->format('d'));
-        $calc_year = intval($calc_date->format('Y'));
+        $start_year = date('Y', $start_date);
+        $start_day = date('d', $start_date);
+        $calc_year = date('Y', $calc_date);
         $prev_year = $start_date;
-        $stopped = true;
-        // Last day in last year.
+        $year_before = false;
+        // Last month in year.
         if($calc_year - $start_year > 0)
         {
-            $stopped = false;
-            $prev_year = new \DateTime(($calc_year - 1).'-12-'.$start_day);
+            $year_before = true;
+            $prev_year = strtotime(($calc_year - 1).'-12-'.$start_day);
         }
 
-        $number_of_m = $this->workedMonth($prev_year, $calc_date);
-        if($stopped){
+        $num_of_m = $this->workedMonth($prev_year, $calc_date);
+        if(!$year_before){
             // Granted vacation on the first working day.
-            $vacation_months = $this->qualified === true ? self::VACATION_FULL_YEAR : $number_of_m * self::VACATION_PER_MONTH + self::VACATION_PER_MONTH;
+            $num_of_vacation = $num_of_m + 1 > self::PROBATION_MONTH_PERIOD ? self::VACATION_FULL_YEAR : $num_of_m * self::VACATION_PER_MONTH + self::VACATION_PER_MONTH;
         }else{
-            $vacation_months = $this->qualified === true ? self::VACATION_FULL_YEAR : $number_of_m * self::VACATION_PER_MONTH;
+            $num_of_vacation = $this->qualified === true ? self::VACATION_FULL_YEAR : $num_of_m * self::VACATION_PER_MONTH;
         }
 
-        $this->vacation['vacation'][$calc_year] = $vacation_months;
-        if($stopped){
-            return $vacation_months;
+        $this->addVacation($calc_year, $num_of_vacation);
+        if(!$year_before){
+            return $num_of_vacation;
         }
 
-        return $vacation_months + $this->accumulatedVacation($start_date, $prev_year);
+        return $num_of_vacation + $this->accumulatedVacation($start_date, $prev_year);
     }
 
     /**
-     * @param \DateTime $start_date
-     * @param \DateTime $calc_date
-     * @return int
+     * @param $start_date
+     * @param $calc_date
+     * @return mixed
      */
-    private function workedMonth(\DateTime $start_date, \DateTime $calc_date)
+    private function workedMonth($start_date, $calc_date)
     {
-        $interval = $start_date->diff($calc_date);
-        $m = intval($interval->format('%m'));
-        $y = intval($interval->format('%y'));
+        $dateDiff = $this->dateDiff($start_date, $calc_date);
+        $y = $dateDiff['year'];
+        $m = $dateDiff['month'];
         if($y > 0){
             $m += $y*12;
         }
@@ -136,19 +194,82 @@ class Vacation {
     }
 
     /**
-     * @param \DateTime $start_date
-     * @param \DateTime $calc_date
+     * @param $time1
+     * @param $time2
+     * @param int $precision
+     * @return array
+     */
+    private function dateDiff($time1, $time2, $precision = 6) {
+
+        // If not numeric then convert texts to unix timestamps
+        if (!is_int($time1)) {
+            $time1 = strtotime($time1);
+        }
+        if (!is_int($time2)) {
+            $time2 = strtotime($time2);
+        }
+
+        // If time1 is bigger than time2
+        // Then swap time1 and time2
+        if ($time1 > $time2) {
+            $ttime = $time1;
+            $time1 = $time2;
+            $time2 = $ttime;
+        }
+
+        // Set up intervals and diffs arrays
+        $intervals = array('year','month','day','hour','minute','second');
+        $diffs = array();
+
+        // Loop through all intervals
+        foreach ($intervals as $interval) {
+            // Create temp time from time1 and interval
+            $ttime = strtotime('+1 ' . $interval, $time1);
+            // Set initial values
+            $add = 1;
+            $looped = 0;
+            // Loop until temp time is smaller than time2
+            while ($time2 >= $ttime) {
+                // Create new temp time from time1 and interval
+                $add++;
+                $ttime = strtotime("+" . $add . " " . $interval, $time1);
+                $looped++;
+            }
+
+            $time1 = strtotime("+" . $looped . " " . $interval, $time1);
+            $diffs[$interval] = $looped;
+        }
+
+        $count = 0;
+        $times = array();
+        // Loop through all diffs
+        foreach ($diffs as $interval => $value) {
+            // Break if we have needed precission
+            if ($count >= $precision) {
+                break;
+            }
+
+            // Add value and interval to times array
+            $times[$interval] = $value;
+            $count++;
+        }
+
+        // Return string with times
+        return $times;
+    }
+
+    /**
+     * @param $start_date
+     * @param $calc_date
      * @return float|mixed
      */
-    private function weekdays(\DateTime $start_date, \DateTime $calc_date)
+    private function weekdays($start_date, $calc_date)
     {
-        $start_timestamp = $start_date->getTimestamp();
-        $calc_timestamp = $calc_date->getTimestamp();
         // Find day of week for 2 dates
-        $start_d = date('N', $start_timestamp);
-        $calc_d = date('N', $calc_timestamp);
+        $start_d = date('N', $start_date);
+        $calc_d = date('N', $calc_date);
         // Calculate the number of time span in terms of weeks.
-        $w = floor(($calc_timestamp - $start_timestamp)/(86400*7));
+        $w = floor(($calc_date - $start_date)/(86400*7));
         // Deduct the first week from the number of weeks.
         if($calc_d >=$start_d)
         {
